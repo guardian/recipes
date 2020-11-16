c890f37db3157c8ec72bf0c769990f93bf1a7bd3
Michel S <64908337+michel-ds@users.noreply.github.com>
2020-11-16 09:26:36 +0000

    Removed obsolete code.

--- recipes-client/components/store.tsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import recipeReducer from '~components/recipe-slice'
-
-export default configureStore({
-  reducer: {
-    counter: recipeReducer
-  }
-})
