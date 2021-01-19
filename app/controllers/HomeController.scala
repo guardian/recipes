01e9912b7aedd995e639db66d3f35e75dda9a682
Michel S <64908337+michel-ds@users.noreply.github.com>
2021-01-19 13:54:08 +0000

    Undoing auth removal.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -15,15 +15,14 @@ class HomeController (
   override val controllerComponents: ControllerComponents,
   val publicSettings: PublicSettings,
   val config: Config
-) extends BaseController with Logging {
-  //with PanDomainAuthentication {
+) extends BaseController with Logging with PanDomainAuthentication {
   /**
    * Create an Action to render an HTML page with a welcome message.
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index(id: String) = Action { //ApiAuthAction {
+  def index(id: String) = ApiAuthAction {
     logger.info("hello there")
     Ok(views.html.index())
   }
