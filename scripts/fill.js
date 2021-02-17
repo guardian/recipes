b5b823473b0f516ca062eceec0afb36696b5e6f2
Michel S <64908337+michel-ds@users.noreply.github.com>
2021-02-17 09:37:47 +0000

    Add script to fill local DB with sample data.

---
+++ scripts/fill.js
@@ -0,0 +1,78 @@
+/*  
+  Fill local Docker-based DB with data from db/data.json
+  Runs as part of the `start` script under `scripts/`
+*/
+
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+
+// Load credentials
+var credentials = new AWS.SharedIniFileCredentials({profile: 'composer'});
+AWS.config.credentials = credentials;
+
+// Set the region 
+AWS.config.update({region: 'eu-west-1', 'endpoint': 'http://localhost:8000'});
+
+// Read file with data
+const fs = require('fs');
+let rawdata = JSON.parse(fs.readFileSync('db/data.json'));
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+var params = {
+  AttributeDefinitions: [
+    {
+      AttributeName: 'recipe_id',
+      AttributeType: 'S'
+    }
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'recipe_id',
+      KeyType: 'HASH'
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5
+  },
+  TableName: 'recipes',
+  StreamSpecification: {
+    StreamEnabled: false
+  }
+};
+
+// Format data
+function formatData(dat, tableName) {
+  return dat.map((item) => {
+    return {
+      TableName: tableName,
+      Item: {...item}
+    }
+  });
+};
+
+// Call DynamoDB to create the table
+ddb.createTable(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Table Created, filling with max 100 data points...");
+
+    rawdata.slice(-100).forEach((item) => {
+      let params = {
+        TableName: "recipes",
+        Item: AWS.DynamoDB.Converter.marshall(item)
+      }
+    
+      ddb.putItem(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item", item.recipe_id, ". Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("PutItem succeeded:", item.recipe_id);
+        }
+      })
+   });
+  }
+});
