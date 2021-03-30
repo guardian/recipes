87071056a625f6440ede227d28c69e8d56b4d891
Michel S <64908337+michel-ds@users.noreply.github.com>
2021-03-30 10:56:39 +0100

    Added main section display, colourMap improvments.

--- recipes-client/components/gu-capi-frame.tsx
+++ recipes-client/components/gu-capi-frame.tsx
@@ -2,10 +2,11 @@
 import { jsx, Global } from "@emotion/core";
 import React from 'react';
 import { HighlightHTML, HighlightPlainText} from "~components/comment-highlighter";
-import { GuCAPIProps, recipeFields} from '~components/interfaces'
+import { allRecipeFields, GuCAPIProps, recipeFields, recipeItem, schemaArrayItem, schemaItem, schemaType} from '~interfaces/main'
 import { getHighlights, DOMParse } from "~utils/html-parsing";
-import {defaultHighlightColours} from "~consts/index";
-import {filterKeys, filterOutKeys} from "~utils/filter";
+import { defaultHighlightColours, excludeInForm, bylineFields } from "~consts/index";
+import { filterKeys, filterOutKeys } from "~utils/filter";
+import { getSchemaType } from "~utils/schema";
 
 function onHighlightMount(id: string, top: number, elem: HTMLElement): void {
   console.debug(`${id} highlight mount.`)
@@ -16,22 +17,63 @@ function focusComment(id: string): void {
   console.log(`${id} focussed.`)
 };
 
+function isSchemaArray(obj: number|string|string[]|recipeItem|schemaItem|schemaType): obj is schemaArrayItem {
+  if (obj === null | typeof obj === 'string') { return false };
+  return "items" in Object.keys(obj);
+} 
+
+// function isSchemaArray<T extends {item?: any}>(obj: T): obj is schemaArrayItem {
+//   return typeof obj.item === "object";
+// } 
+
+
+// Todo: Move to better place
+function flattenSchema(schema: schemaType): string[]{
+  // Get all keys from `schema` and output list of them
+  // For nested keys, create a composite key (level1.level1)
+  const schemaProp = schema.properties;
+  return (Object.keys(schemaProp) as Array<keyof allRecipeFields>).reduce((acc, key) => {
+    const schemaEl = schemaProp[key];
+    if (isSchemaArray(schemaEl)){
+      if (Object.keys(schemaEl.items).includes("type") && 
+          getSchemaType(schemaEl.items.type).includes('object')) {
+        const compositeKeys = Object.keys(
+          schemaEl.items.properties
+          ).map(i => `${key}.${i}`);
+        return [...acc, ...compositeKeys]
+      }
+    }
+    return [...acc, key];
+  }, [] as string[])
+}
+
+function createColourMap(keyList: string[], excludeItems: string[] , colours: string[]): Record<string, string>{
+  return keyList.reduce((acc, key, i) => {
+    if (!excludeItems.includes(key)) {
+      acc[key]= colours[i % colours.length];
+    }
+    return acc
+  }, {} as Record<string, string>)
+}
+
 function GuCAPIFrame(props: GuCAPIProps): JSX.Element {
-  const {isLoading, recipeItems, html, colours} = props;
-  if (isLoading || recipeItems === null){
+  const {isLoading, recipeItems, html, schema, colours} = props;
+  if (isLoading || recipeItems === null || schema === null){
     return <h3> LOADING... </h3>
   } else {
     const cols = (colours === null || colours === undefined) ? defaultHighlightColours : colours;
-    const colourMap = Object.keys(recipeItems).reduce((acc, key, i) => {
-      acc[key] = cols[i % cols.length];
-      return acc
-    }, {} as typeof recipeItems)
+    // const colourMap = Object.keys(recipeItems).reduce((acc, key, i) => {
+    //   acc[key] = cols[i % cols.length];
+    //   return acc
+    // }, {} as typeof recipeItems)
+    const keyList = flattenSchema(schema);
+    const colourMap = createColourMap(keyList, excludeInForm, cols);
 
     const doc = DOMParse(html['fields']['body'])
     const byline = DOMParse(html['fields']['byline'])
-    const recipeItemsByline = filterKeys((recipeItems as Record<string, unknown>), ['credit']) as recipeFields;
+    const recipeItemsByline = filterKeys((recipeItems as Record<string, unknown>), bylineFields) as recipeFields;
     const bylineHighlights = getHighlights(byline, recipeItemsByline);
-    const recipeItemsBody = filterOutKeys((recipeItems as Record<string, unknown>), ['credit', 'recipeId', 'image']) as recipeFields;
+    const recipeItemsBody = filterOutKeys((recipeItems as Record<string, unknown>), excludeInForm) as recipeFields;
     const highlights = getHighlights(doc, recipeItemsBody);
 
     return (
@@ -46,6 +88,7 @@ function GuCAPIFrame(props: GuCAPIProps): JSX.Element {
           height: 'auto',
         }
       }} />
+        <p dangerouslySetInnerHTML={{__html: html.fields.main}} />
         <HighlightHTML  html={doc} highlights={highlights} focusedId="" onHighlightMount={onHighlightMount} focusComment={focusComment} colours={colourMap} />
     </>
     )
--- recipes-client/consts/index.ts
+++ recipes-client/consts/index.ts
@@ -1,6 +1,7 @@
-export const articlePath =
-  "/food/2019/mar/02/yotam-ottolenghi-north-african-recipes-tunisian-pepper-salad-moroccan-chicken-pastilla-umm-ali-pudding"
+// export const articlePath =
+//   "/food/2019/mar/02/yotam-ottolenghi-north-african-recipes-tunisian-pepper-salad-moroccan-chicken-pastilla-umm-ali-pudding"
   // "/food/2020/aug/29/salad-starter-side-yotam-ottolenghi-recipes-courgettes-yoghurt-saffron-butter-tomatoes-ricotta-fennel-preserved-lemon";
+/* Routing information */
 export const apiURL = "/api/curation/"
 export const capiProxy = "/api/capi/"
 export const listEndpoint = "/api/list"
@@ -13,7 +14,9 @@ export const urls = {
   matches: "check"
 }
 
+/* UI display output config */
 export const newColours = ["aquamarine", "blue", "brown", "coral", "yellow", "chartreuse", "darkseagreen"];
 export const defaultHighlightColours   = ["#E69F0090", "#56B4E990", "#009E7390", "#F0E44290", "#0072B290", "#D55E0090", "#CC79A790"]
 
-export const excludeInForm = ['image'];
+export const excludeInForm = ['credit', 'recipeId', 'image', "ingredient_tag"];
+export const bylineFields = ['credit'];
