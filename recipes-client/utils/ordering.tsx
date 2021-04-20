87d8ac214975a849e92bde41fa04d9273b8cdd31
Michel S <64908337+michel-ds@users.noreply.github.com>
2021-04-20 17:00:54 +0100

    Safeguard if input is null.

--- recipes-client/utils/ordering.tsx
+++ recipes-client/utils/ordering.tsx
@@ -2,6 +2,7 @@ import { allRecipeFields, recipeItem } from "~interfaces/main";
 import fromPairs from "lodash-es/fromPairs";
 
 export function orderComponents(rc: allRecipeFields, sortOrder: string[]) {
+  if (rc === null) {return null;}
   return fromPairs(
     sortOrder.map((key: keyof allRecipeFields) => {
       const value = rc[key];
