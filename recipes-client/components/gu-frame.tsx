4d59c546d052db34b252aa91b5c8bd5d431297f7
frederickobrien <frederick.obrien@guardian.co.uk>
2023-10-03 16:38:46 +0100

    Get proxy iframe working again
    Slightly nasty workaround to remove ids from the path but that should sort itself out in due course

--- recipes-client/components/gu-frame.tsx
+++ recipes-client/components/gu-frame.tsx
@@ -3,10 +3,18 @@
 import { getProxyPath } from '../utils/proxy';
 
 export default (props: { articlePath: string }): JSX.Element => {
-	const proxyPath = getProxyPath(props.articlePath);
+	// Workaround until we remove recipe number from path
+	const trimmedPath = props.articlePath.slice(0, -2);
+	const proxyPath = getProxyPath(trimmedPath);
 	return (
 		<iframe
-			css={{ border: 'none', width: '100%', height: '100%' }}
+			css={{
+				border: 'solid lightgray 2px',
+				borderWidth: '30px 20px 60px 20px',
+				borderRadius: '10px',
+				width: '90%',
+				height: '90%',
+			}}
 			src={proxyPath}
 		></iframe>
 	);
