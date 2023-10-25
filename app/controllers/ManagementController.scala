66fc4375bae183a0bff6facb60a55f6df5ad4f7a
Michel <64908337+michel-ds@users.noreply.github.com>
2023-10-25 16:20:19 +0100

    Merge pull request #45 from guardian/ms/basecamp-refactoring
    Remove old BuildInfo

--- app/controllers/ManagementController.scala
+++ app/controllers/ManagementController.scala
@@ -5,9 +5,6 @@ import play.api.libs.json.Json
 import play.api.mvc.{BaseController, ControllerComponents}
 
 class ManagementController (override val controllerComponents: ControllerComponents) extends BaseController with Logging {
-  def manifest = Action {
-    Ok(Json.parse(recipes.BuildInfo.toJson))
-  }
 
   def disallowRobots = Action {
     Ok("User-agent: *\nDisallow: /\n")
