ade1103bd21826bc686a48efc0b577425fcf8a6b
Tom Richards <tom.richards@guardian.co.uk>
2023-11-21 22:26:31 +0000

    Automatically track in workflow if not already tracked and pre-select corresponding pinboard if/once tracked (encapsulated in new component `PinboardTrackAndPreselect`)
    Co-authored-by: Frederick O'Brien <frederick.obrien@guardian.co.uk>

---
+++ recipes-client/components/curation/pinboard-track-and-preselect.tsx
@@ -0,0 +1,73 @@
+/** @jsxImportSource @emotion/react */
+import { useEffect, useState } from 'react';
+
+interface PinboardTrackAndPreselectProps {
+	maybeComposerId: string | undefined;
+	maybeTitle: string | undefined;
+}
+
+export const PinboardTrackAndPreselect = ({
+	maybeComposerId,
+	maybeTitle,
+}: PinboardTrackAndPreselectProps) => {
+	const [maybeWorkflowId, setMaybeWorkflowId] = useState<string>();
+
+	useEffect(() => {
+		(async () => {
+			if (!maybeComposerId) return;
+			const workflowDomain = window.location.host
+				.replace('recipes', 'workflow')
+				.replace('.local.', '.code.');
+			const workflowContentUrl = `https://${workflowDomain}/api/content`;
+			const workflowLookupResponse = await fetch(
+				`${workflowContentUrl}/${maybeComposerId}`,
+				{
+					credentials: 'include',
+				},
+			);
+			if (workflowLookupResponse.ok) {
+				return setMaybeWorkflowId(
+					(await workflowLookupResponse.json()).data.id,
+				);
+			} else if (workflowLookupResponse.status === 419) {
+				console.error(
+					"Not logged in to Workflow, Pinboard won't be pre-selected",
+				);
+			} else if (workflowLookupResponse.status === 404) {
+				const trackInWorkflowResponse = await fetch(workflowContentUrl, {
+					method: 'POST',
+					credentials: 'include',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						composerId: maybeComposerId,
+						title:
+							window.prompt(
+								'Please enter the working title for workflow?',
+								maybeTitle,
+							) || maybeTitle,
+						status: 'Writers', //TODO is Subs a more sensible starting status
+						section: { name: 'Recipes Data' },
+						contentType: 'article', // TODO is this one actually needed
+						prodOffice: 'UK', //TODO is everything going to be UK based??
+					}),
+				});
+				if (trackInWorkflowResponse.ok) {
+					return setMaybeWorkflowId(
+						(await trackInWorkflowResponse.json()).data.stubId,
+					);
+				}
+			}
+			console.error(
+				"Failed looking up in workflow, pinboard won't be pre-selected",
+				workflowLookupResponse,
+			);
+		})();
+	}, [maybeComposerId]);
+
+	return maybeWorkflowId ? (
+		// @ts-ignore -- this element is not valid JSX but IS meaningful/detected by Pinboard
+		<pinboard-preselect data-composer-id={maybeComposerId}></pinboard-preselect>
+	) : null;
+};
