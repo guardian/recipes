758bbf0eda0005154bb37dbddaed011ffc435907
frederickobrien <frederick.obrien@guardian.co.uk>
2023-12-13 14:57:23 +0000

    Display top chef recipe counts

--- recipes-client/components/dashboard/recipes-overview.tsx
+++ recipes-client/components/dashboard/recipes-overview.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { RecipeListType } from './recipe-list';
 
 interface RecipesOverviewProps {
@@ -24,30 +26,96 @@ export const RecipesOverview = ({ recipesList }: RecipesOverviewProps) => {
 			name: 'Meera Sodha',
 			contributorTag: 'profile/meera-sodha',
 		},
+		{
+			name: 'Anna Jones',
+			contributorTag: 'profile/anna-jones',
+		},
+		{
+			name: 'Thomasina Miers',
+			contributorTag: 'profile/thomasina-miers',
+		},
+		{
+			name: 'Nigella Lawson',
+			contributorTag: 'profile/nigella-lawson',
+		},
+		{
+			name: 'Rachel Roddy',
+			contributorTag: 'profile/rachel-roddy',
+		},
+		{
+			name: 'Benjamina Ebuehi',
+			contributorTag: 'profile/benjamina-ebuehi',
+		},
+		{
+			name: 'Ravneet Gill',
+			contributorTag: 'profile/ravneet-gill',
+		},
+		{
+			name: 'Jose Pizarro',
+			contributorTag: 'profile/jose-pizarro',
+		},
+		{
+			name: 'Rukmini Iyer',
+			contributorTag: 'profile/rukmini-iyer',
+		},
 	];
 
-	const getAuthorCount = (contributorTag: string) => {
-		return appReadyRecipes.reduce(
+	const countOfRecipesByOtherChefs = appReadyRecipes.reduce(
+		(acc, recipe) =>
+			recipe.contributors.some(
+				(contributor) =>
+					!chefs.map((chef) => chef.contributorTag).includes(contributor),
+			)
+				? ++acc
+				: acc,
+		0,
+	);
+	const authorSummaryCard = (name: string, contributorTag: string) => {
+		const chefRecipeCount = appReadyRecipes.reduce(
 			(acc, recipe) =>
 				recipe.contributors.includes(contributorTag) ? ++acc : acc,
 			0,
 		);
-	};
-	const authorSummaryCard = (name: string, contributorTag: string) => {
-		const count = getAuthorCount(contributorTag);
-		const percentageOfAllRecipes = (count / appReadyRecipes.length) * 100;
+		const percentOfAllRecipes = Math.round(
+			(chefRecipeCount / appReadyRecipes.length) * 100,
+		);
 		return (
-			<>
-				<div>
-					{name}: {getAuthorCount(contributorTag)} ({percentageOfAllRecipes}%)
-				</div>
-			</>
+			<div css={infoBoxStyles}>
+				<strong>{name}:</strong> {chefRecipeCount} ({percentOfAllRecipes}%)
+			</div>
 		);
 	};
 	return (
 		<>
-			<h2>{appReadyRecipes.length} app-ready recipes</h2>
-			{chefs.map((chef) => authorSummaryCard(chef.name, chef.contributorTag))}
+			<h3>
+				{appReadyRecipes.length} app-approved recipes, which break down as
+				follows...
+			</h3>
+			<div
+				css={css`
+					display: flex;
+					flex-direction: row;
+					flex-wrap: wrap;
+				`}
+			>
+				{chefs.map((chef) => authorSummaryCard(chef.name, chef.contributorTag))}
+				<div css={infoBoxStyles}>
+					<strong>Other chefs:</strong> {countOfRecipesByOtherChefs} (
+					{Math.round(
+						(countOfRecipesByOtherChefs / appReadyRecipes.length) * 100,
+					)}
+					%)
+				</div>
+			</div>
 		</>
 	);
 };
+
+const infoBoxStyles = css`
+	padding: 10px;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	margin-bottom: 10px;
+	margin-right: 10px;
+	max-width: 300px;
+`;
--- recipes-client/pages/home.tsx
+++ recipes-client/pages/home.tsx
@@ -8,6 +8,7 @@ import RecipeList, {
 	RecipeListType,
 } from '../components/dashboard/recipe-list';
 import { listEndpoint } from '../consts/index';
+import { WelcomeExplainer } from 'components/dashboard/welcome-explainer';
 
 const Home = (): JSX.Element => {
 	const [recipeList, setList] = useState<RecipeListType[]>([]);
@@ -64,25 +65,28 @@ const Home = (): JSX.Element => {
 					app-approved recipes and counting
 				</div>
 			</div>
-			<div css={explainerStyles}>
-				<h2>This is it how it works:</h2>
-				<ul>
-					<li>Pick a recipe.</li>
-					<li>Edit it.</li>
-					<li>Save it.</li>
-				</ul>
-				<div>
-					Read the{' '}
-					<a
-						href="https://docs.google.com/document/d/1wrVUX7vVTLBd0fxqDbQxqmyXoY4Vvyw5aX79tMxROhk/edit#heading=h.7n6l8nswve9p"
-						target="_blank"
-					>
-						recipe data curation guide
-					</a>{' '}
-					for a more in-depth breakdown.
+			<div
+				css={css`
+					display: flex;
+					flex-direction: row;
+					justify-content: space-between;
+				`}
+			>
+				<div
+					css={css`
+						width: 33%;
+					`}
+				>
+					<WelcomeExplainer />
+				</div>
+				<div
+					css={css`
+						width: 67%;
+					`}
+				>
+					<RecipesOverview recipesList={recipeList} />
 				</div>
 			</div>
-			<RecipesOverview recipesList={recipeList} />
 			<hr />
 			<div>
 				<RadioGroup orientation="horizontal" label="Filter displayed recipes">
@@ -128,8 +132,4 @@ export default Home;
 
 const mainContainerStyles = css`
 	padding: 20px;
-`;
-
-const explainerStyles = css`
-	margin-bottom: 30px;
 `;
